1836184dfcceabd96d155449487856e7de114b5b
RomanKoronovskyi <102824796+RomanKoronovskyi@users.noreply.github.com>
2026-02-07 18:02:18 +0200

    Create grafana_setup.sh

---
+++ grafana_setup.sh
@@ -0,0 +1,20 @@
+!#/bin/bash
+#documentation: https://grafana.com/grafana/download
+set -e
+
+VERSION="12.3.2"
+SERVICE="grafana-server"
+URL="https://dl.grafana.com/grafana-enterprise/release/${VERSION}/grafana-enterprise_${VERSION}_21390657659_linux_amd64.deb"
+DEB="grafana-enterprise_${VERSION}_21390657659_linux_amd64.deb"
+
+sudo apt update && sudo apt upgrade -y
+sudo apt-get install -y adduser libfontconfig1 musl
+wget "${URL}"
+sudo dpkg -i "${DEB}"
+
+sudo systemctl daemon-reload
+sudo systemctl enable "${SERVICE}"
+sudo systemctl start "${SERVICE}"
+
+echo "Config: /etc/grafana/grafana.ini"
+echo "Data: /var/lib/grafana/"
